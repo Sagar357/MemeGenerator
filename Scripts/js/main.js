e99e3e1341e3cadf66090b7680a0e4a2a54cbf3a
himanshu8318@outlook.com <himanshu8318@outlook.com>
2021-01-08 15:12:58 -0800

    commit

--- Scripts/js/main.js
+++ Scripts/js/main.js
@@ -37,7 +37,7 @@ $(document).ready(function(){
   
     //----end---here----//
  
-document.getElementById('previewImage').onchange = function (e) {
+$('#hiddenFileUpload').onchange = function (e) {
     debugger
     var img = new Image();
     img.onload = draw;
@@ -57,23 +57,23 @@ function failed() {
     console.error("The provided file couldn't be loaded as an Image media");
 }
 
-function convertImgToBase64URL(url, callback, outputFormat) {
-    var img = new Image();
-    img.crossOrigin = 'Anonymous';
-    img.onload = function () {
-        var canvas = document.createElement('CANVAS'),
-            ctx = canvas.getContext('2d'), dataURL;
-        canvas.height = img.height;
-        canvas.width = img.width;
-        ctx.drawImage(img, 0, 0);
-        dataURL = canvas.toDataURL(outputFormat);
-        callback(dataURL);
-        canvas = null;
-    };
-    img.src = url;
-}
-convertImgToBase64URL('https://cdn.evilmartians.com/front/posts/optimizing-react-virtual-dom-explained/cover-a1d5b40.png', function (base64Img) {
-    alert('it works');
-    $('.output').find('img').attr('src', base64Img);
+//function convertImgToBase64URL(url, callback, outputFormat) {
+//    var img = new Image();
+//    img.crossOrigin = 'Anonymous';
+//    img.onload = function () {
+//        var canvas = document.createElement('CANVAS'),
+//            ctx = canvas.getContext('2d'), dataURL;
+//        canvas.height = img.height;
+//        canvas.width = img.width;
+//        ctx.drawImage(img, 0, 0);
+//        dataURL = canvas.toDataURL(outputFormat);
+//        callback(dataURL);
+//        canvas = null;
+//    };
+//    img.src = url;
+//}
+//convertImgToBase64URL('https://cdn.evilmartians.com/front/posts/optimizing-react-virtual-dom-explained/cover-a1d5b40.png', function (base64Img) {
+//    alert('it works');
+//    $('.output').find('img').attr('src', base64Img);
 
-});
+//});
