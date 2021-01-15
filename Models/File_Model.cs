515380f6e01b8eed0659eb06cd577a18c64341b4
himanshu8318@outlook.com <himanshu8318@outlook.com>
2021-01-15 15:52:31 -0800

    commit

--- Models/File_Model.cs
+++ Models/File_Model.cs
@@ -27,6 +27,18 @@ namespace MemeGenerator.Models
 
     }
 
+    public class memecontact
+    {
+        public int customerId { get; set; }
+        public string customerName { get; set; }
+        public string customerMobile { get; set; }
+        public string customerMessage { get; set; }
+        public string customerEmail { get; set; }
+
+        public string createdDate { get; set; }
+        public string modifiedDate { get; set; }
+        public string isActive { get; set; }
+    }
     //public class ImageModification_Model
     //{
     //    public string filePath { get; set; }
