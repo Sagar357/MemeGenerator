2a072c666d24799d640e41e408423aec22c6e33a
Himanshu2210 <75522679+Himanshu2210@users.noreply.github.com>
2021-01-30 19:59:58 -0800

    co

--- Scripts/js/main.js
+++ Scripts/js/main.js
@@ -59,18 +59,18 @@ function failed() {
 }
 // inspect block code start//
 
- $(document).keydown(function(event){
-  if(event.keyCode==123){
-       return false;
-   }
-   else if (event.ctrlKey && event.shiftKey && event.keyCode==73){        
-            return false;
-   }
- });
+ //$(document).keydown(function(event){
+ // if(event.keyCode==123){
+ //      return false;
+ //  }
+ //  else if (event.ctrlKey && event.shiftKey && event.keyCode==73){        
+ //           return false;
+ //  }
+ //});
 
- $(document).on("contextmenu",function(e){        
-  e.preventDefault();
- });
+ //$(document).on("contextmenu",function(e){        
+ // e.preventDefault();
+ //});
 // end inspect block code//
 
 //start control u source code block//
