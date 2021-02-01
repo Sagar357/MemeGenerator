d17c04b62f074808a84f6ddfd86454432632f15a
@Sagar357 <markushno357@gmail.com>
2021-02-01 19:19:42 +0530

    commit

--- Scripts/js/main.js
+++ Scripts/js/main.js
@@ -105,3 +105,66 @@ document.onkeydown = function (e) {
 //    $('.output').find('img').attr('src', base64Img);
 
 //});
+
+
+
+function dragElement(elmnt) {
+    debugger
+    var pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
+    if (document.getElementById(elmnt.id + "header")) {
+        // if present, the header is where you move the DIV from:
+        document.getElementById(elmnt.id + "header").onmousedown = dragMouseDown;
+    } else {
+        // otherwise, move the DIV from anywhere inside the DIV:
+        elmnt.onmousedown = dragMouseDown;
+    }
+
+    function dragMouseDown(e) {
+        e = e || window.event;
+        e.preventDefault();
+        // get the mouse cursor position at startup:
+        pos3 = e.clientX;
+        pos4 = e.clientY;
+        document.onmouseup = closeDragElement;
+        // call a function whenever the cursor moves:
+        document.onmousemove = elementDrag;
+    }
+
+    function elementDrag(e) {
+        e = e || window.event;
+        e.preventDefault();
+        // calculate the new cursor position:
+        pos1 = pos3 - e.clientX;
+        pos2 = pos4 - e.clientY;
+        pos3 = e.clientX;
+        pos4 = e.clientY;
+        // set the element's new position:
+        elmnt.style.top = (elmnt.offsetTop - pos2) + "px";
+        elmnt.style.left = (elmnt.offsetLeft - pos1) + "px";
+    }
+
+    function closeDragElement() {
+        // stop moving when mouse button is released:
+        document.onmouseup = null;
+        document.onmousemove = null;
+    }
+}
+
+var dragele = document.getElementsByClassName("drag-box")
+for (var i = 0; i < dragele.length; i++) {
+    dragElement(document.getElementById(dragele[i].id));
+}
+//dragele.forEach(function (item, index) {
+//    debugger
+//    dragElement(document.getElementsById(item.id));
+//});
+
+//function mouseMove(e) {
+//    debugger
+  
+//    var box = $(".drag-box");
+//    var boxCenter = [box.offset().left + box.width() / 2, box.offset().top + box.height() / 2];
+
+//    var angle = Math.atan2(e.pageX - boxCenter[0], - (e.pageY - boxCenter[1])) * (180 / Math.PI);      
+
+//}
